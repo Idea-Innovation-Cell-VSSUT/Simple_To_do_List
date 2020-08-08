94b2c202881575c42fda96a198d686e035deead7
patelaryan7751 <59426397+patelaryan7751@users.noreply.github.com>
2020-08-09 02:17:58 +0530

    done

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 // jshint esversion:6
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCSEiwHD5jBfH3zo5eb91tZSnY3ngdRXnk",
+    authDomain: "party-493df.firebaseapp.com",
+    databaseURL: "https://party-493df.firebaseio.com",
+    projectId: "party-493df",
+    storageBucket: "party-493df.appspot.com",
+    messagingSenderId: "792926467414",
+    appId: "1:792926467414:web:f173aa76e6843ab78c92f7",
+    measurementId: "G-E20LGG6G94"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
 
 const addForm = document.querySelector(".add");
 
@@ -9,28 +23,39 @@ const search = document.querySelector(".search input");
 
 
 // add todos
-const generateTemplate = todo => {
 
-    var html = `
+var hjcordiref= firebase.database().ref("todos");
+     hjcordiref.orderByChild("todo").on("child_added", function(data){
+          var newVoke= data.val();
+         
+         var todo1 =newVoke.todo;
+         var html = `
         <li class="list-group-item d-flex justify-content-between align-items-center">
-            <span>${todo}</span>
-            <i class="far fa-trash-alt delete"></i>
+            <span>${todo1}</span>
+            <i class="fa fa-trash-alt delete" data-id='${data.key}' onclick='deleteMessage(this);'></i>
         </li>
     `;
+         var ele=document.getElementById("elem");
+    ele.innerHTML += html;
+html="";
+         
+          });
+    
 
-    list.innerHTML += html;
 
-};
+
 
 addForm.addEventListener("submit", e => {
 
     e.preventDefault();
+    var joinl = document.querySelector('#add').value;
+        var tgref=firebase.database().ref("todos");
+    var data={
+        todo:joinl
+         }
+    tgref.push(data);
 
-    var todos = addForm.add.value.trim();
-    if(todos.length) {
-        generateTemplate(todos);
-        addForm.reset();
-    }
+   console.log(addForm);
 
 });
 
@@ -44,6 +69,13 @@ list.addEventListener("click", e => {
     }
 
 });
+function deleteMessage(self){
+    
+     var todoId = self.getAttribute("data-id");
+ 
+    // delete message
+    firebase.database().ref("todos/").child(todoId).remove();
+}
 
 // search todos
 const fiterTodo = term => {
